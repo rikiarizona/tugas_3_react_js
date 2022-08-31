35204c937eb1617161c6cfdd181d64c3d273e6af
Riki Arizona <rikiarizona@gmail.com>
2022-08-31 19:43:15 +0700

    TUGAS_3_REACT_JS

---
+++ src/Page/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <center>
+                <b>Home </b>|
+                <b> Product </b>|
+                <b> Kontak </b>|
+                <b> Tentang Kami </b>
+            </center>
+        </div>
+    )
+}
+
+export default Header;
